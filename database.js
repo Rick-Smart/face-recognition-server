c4df0d74b1a212cc54fae08b6b2f8fd46b22b339
Xis <rick-s@hotmail.com>
2021-11-15 18:43:48 -0800

    added DB config for mysql

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 const db = knex({
   client: process.env.DB_CLIENT || "mysql",
   connection: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "us-cdbr-east-04.cleardb.com",
     port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    user: process.env.DB_USER || "b3c41a6f0578c7",
+    password: process.env.DB_PASSWORD || "de3623f6",
+    database: process.env.DB || "heroku_4759242a8f057b9",
   },
 });
 
