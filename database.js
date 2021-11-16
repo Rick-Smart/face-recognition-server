9b3c24a021c0fb0ce9e9cfb9bf260d83aa70982a
Xis <rick-s@hotmail.com>
2021-11-15 19:21:51 -0800

    using mysql env for db connection

--- database.js
+++ database.js
@@ -4,11 +4,7 @@ require("dotenv").config();
 const db = knex({
   client: process.env.DB_CLIENT || "mysql",
   connection: {
-    host: process.env.DB_HOST || "us-cdbr-east-04.cleardb.com",
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER || "b3c41a6f0578c7",
-    password: process.env.DB_PASSWORD || "de3623f6",
-    database: process.env.DB || "heroku_4759242a8f057b9",
+    host: process.env.DATABASE_URL,
   },
 });
 
