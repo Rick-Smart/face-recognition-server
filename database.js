cc43b33e57a9a54ce6d8b1ec2458b83933e593d0
Xis <rick-s@hotmail.com>
2021-11-15 17:15:49 -0800

    working with knex db info to recognize client data

--- database.js
+++ database.js
@@ -2,14 +2,14 @@ const knex = require("knex");
 require("dotenv").config();
 
 const db = knex({
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
-    },
-  });
+  client: process.env.DB_CLIENT || "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+  },
+});
 
-  module.exports = db
+module.exports = db;
