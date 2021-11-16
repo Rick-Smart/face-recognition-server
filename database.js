29a2a94ae5c9ed17d9c443e719dade44083c5e45
Xis <rick-s@hotmail.com>
2021-11-15 19:33:07 -0800

    testing new db config

--- database.js
+++ database.js
@@ -4,8 +4,11 @@ require("dotenv").config();
 const db = knex({
   client: process.env.DB_CLIENT || "mysql",
   connection: {
-    host: process.env.CLEARDB_DATABASE_URL,
-    ssl: true,
+    host: process.env.DB_HOST || "us-cdbr-east-04.cleardb.com",
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER || "b3c41a6f0578c7",
+    password: process.env.DB_PASSWORD || "de3623f6",
+    database: process.env.DB || "heroku_4759242a8f057b9",
   },
 });
 
