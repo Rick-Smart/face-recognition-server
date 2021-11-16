4c1ab95a034774f6b7986a99f8081ca8e295b7c2
Xis <rick-s@hotmail.com>
2021-11-15 20:03:50 -0800

    removed env config vars

--- database.js
+++ database.js
@@ -2,9 +2,13 @@ const knex = require("knex");
 require("dotenv").config();
 
 const db = knex({
-  client: "mysql",
+  client: process.env.DB_CLIENT,
   connection: {
-    host: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 });
 
