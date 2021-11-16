1bbafd5926a9cc4e055b86f1a66317e3e3ad3918
Xis <rick-s@hotmail.com>
2021-11-15 20:14:12 -0800

    working with knex mysql connectionstring

--- database.js
+++ database.js
@@ -4,11 +4,7 @@ require("dotenv").config();
 const db = knex({
   client: process.env.DB_CLIENT,
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
   },
 });
 
