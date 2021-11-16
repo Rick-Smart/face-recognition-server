f63142a218bd56e523568a57bb02a6cf97f71e25
Xis <rick-s@hotmail.com>
2021-11-15 20:30:24 -0800

    added the config vars in heroku and used process.env.ETC to impliment them

--- database.js
+++ database.js
@@ -4,8 +4,11 @@ require("dotenv").config();
 const db = knex({
   client: process.env.DB_CLIENT,
   connection: {
-    connection: process.env.CLEARDB_DATABASE_URL,
-    ssl: true,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 });
 
